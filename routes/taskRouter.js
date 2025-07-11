e50bfbc4ff4690be034d0f81d4d01d0210ffa47c
RohitJM777 <rohit007.com@gmail.com>
2025-07-11 20:52:57 +0530

    change

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -7,7 +7,7 @@ const router=express.Router()
 router.post("/createTask",protect,authorization('admin','manager'),createTask)
 router.get("/getTask",protect,getTask)
 router.get("/getAllTask",protect,getAllTasks)
-
+router.put("/changeStatus",protect,changeStatus)
 router.put("/updateTask",protect,authorization('admin','manager'),updateTask)
 router.delete("/deleteTask",protect,authorization('admin'),deleteTask)
 
